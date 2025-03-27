75274d66b060c2b69c3e723bd76cce67999b3c85
aryanbeni <aryanbeni06@gmail.com>
2025-03-27 13:22:35 +0530

    trying

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -11,16 +11,33 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate to the second screen using a named route.
-            Navigator.pushNamed(context, '/second');
-          },
-          child: const Text('Login'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.indigo,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 12.0),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                      Text('Sign In',style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),),
+                      Text('Sign Up',style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),)                      ],
+                      ),
+                    )
+                  ],
+                ),
+                ),
+            ),
+          ],
         ),
       ),
     );
