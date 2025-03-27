5de4a724e98ebcd255ffa0d3a45a9cdccbd7847e
aryan1univest <aryan.beni@univest.in>
2025-03-27 12:09:52 +0530

    first commit

---
+++ android/app/src/main/java/com/example/srmuh_elearning/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.srmuh_elearning;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/login_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to the second screen using a named route.
+            Navigator.pushNamed(context, '/second');
+          },
+          child: const Text('Login'),
+        ),
+      ),
+    );
+  }
+}
