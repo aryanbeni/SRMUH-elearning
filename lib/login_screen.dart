3290caf44033666894d5a8f1d8e6f3d7730009ed
aryanbeni <aryanbeni06@gmail.com>
2025-03-28 11:55:37 +0530

    Sign in page

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tab_container/tab_container.dart';
 
@@ -9,6 +10,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool passwordVisible=true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,27 +29,9 @@ class _LoginScreenState extends State<LoginScreen> {
                 // ),
                 child: TabContainer(
                   color: Colors.indigo,
-                  tabs: [
-                    Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          border: Border.all(color: Colors.indigo),
-                          color: Colors.indigo,
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 6.0,vertical: 1),
-                          child: Text('Sign In',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),),
-                        )),
-                    Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          border: Border.all(color: Colors.indigo),
-                          color: Colors.indigo,
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 6.0,vertical: 1),
-                          child: Text('Sign Up',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25),),
-                        )),
+                  tabs: const [
+                    Text('Sign In',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 25),),
+                    Text('Sign Up',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 25),),
                   ],
                   children: [
                     Container(
@@ -55,8 +40,8 @@ class _LoginScreenState extends State<LoginScreen> {
                         padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 20),
                         child: Column(
                           children: [
-                            Text('Welcome Back !',style: TextStyle(color: Colors.white,fontSize: 23,fontWeight: FontWeight.bold),),
-                            SizedBox(height: 10),
+                            const Text('Welcome Back !',style: TextStyle(color: Colors.white,fontSize: 23,fontWeight: FontWeight.bold),),
+                            const SizedBox(height: 10),
                             TextField(
                             keyboardType: TextInputType.emailAddress,
                             decoration: InputDecoration(
@@ -72,31 +57,44 @@ class _LoginScreenState extends State<LoginScreen> {
                               ),
                             ),
                           ),
-                            SizedBox(height: 15),
+                            const SizedBox(height: 15),
                             TextField(
+                              keyboardType: TextInputType.visiblePassword,
+                              obscureText: passwordVisible,
                               decoration: InputDecoration(
                                 fillColor: Colors.white.withOpacity(0.8),
                                 filled: true,
                                 hintText: 'Password',
-                                border: OutlineInputBorder(
+                                border: const OutlineInputBorder(
                                   borderSide: BorderSide(color: Colors.white),
                                 ),
                                 focusedBorder: OutlineInputBorder(
                                     borderRadius: BorderRadius.circular(25)),
                                 prefixIcon: const Icon(Icons.lock),
-                                focusColor: Colors.white,
+                                suffixIcon: IconButton(
+                                  icon: Icon(passwordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off),
+                                  onPressed: () {
+                                    setState(
+                                          () {
+                                        passwordVisible = !passwordVisible;
+                                      },
+                                    );
+                                  },
+                                ),
                                 enabledBorder: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(25),
                                 ),
                               ),
                             ),
-                            SizedBox(height: 10),
-                            Row(mainAxisAlignment: MainAxisAlignment.end,
+                            const SizedBox(height: 10),
+                            const Row(mainAxisAlignment: MainAxisAlignment.end,
                               children: [
                                 Text('Forgot Password?',style: TextStyle(color: Colors.white,fontSize: 16),),
                               ],
                             ),
-                            SizedBox(height: 10),
+                            const SizedBox(height: 10),
                             Padding(
                               padding: const EdgeInsets.symmetric(horizontal: 52.0),
                               child: Container(
@@ -132,8 +130,8 @@ class _LoginScreenState extends State<LoginScreen> {
                                   borderRadius: BorderRadius.circular(20),
                                   color: Colors.white,
                                 ),
-                                child: Center(child: Padding(
-                                  padding: const EdgeInsets.symmetric(vertical: 6.0),
+                                child: const Center(child: Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 6.0),
                                   child: Text('Sign in with google',style: TextStyle(fontSize: 16),),
                                 )),
                               ),
